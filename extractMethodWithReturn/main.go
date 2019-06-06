483262e2478f5e242e9316309dd1f61d20e49aa3
Artem Pronichev <artem.pronichev@jetbrains.com>
2019-06-06 13:58:11 +0300

    Extract a method with the return statement

---
+++ extractMethodWithReturn/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Printf("Enter text: ")
+		text, _ := reader.ReadString('\n')
+		text = strings.TrimSpace(text)
+		if text == "quit"{
+			return
+		} else if text == "foo" {
+			fmt.Printf("foo")
+		} else {
+			fmt.Printf("bar")
+		}
+	}
+}

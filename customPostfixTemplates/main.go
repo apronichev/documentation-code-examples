c881fb9cfe1cd7232ea5b89a51dcd382f9d7a076
Artem Pronichev <artem.pronichev@jetbrains.com>
2019-06-10 13:40:24 +0300

    Create a custom postfix template

---
+++ customPostfixTemplates/main.go
@@ -0,0 +1,13 @@
+package main
+
+//Add the following template to Settings/Preferences| Editor | General | Postfix Completion.
+//for i := len($EXPR$) - 1; i >= 0; i-- {
+//$END$
+//}
+
+func main() {
+	a := [...]int{1, 2, 3, 4, 5, 6, 7}
+	for i := len(a) - 1; i >= 0; i-- {
+
+	}
+}

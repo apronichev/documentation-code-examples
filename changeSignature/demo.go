21a398b84119903c92dcd3009771a06e64149cbc
Artem Pronichev <artem.pronichev@jetbrains.com>
2019-06-17 15:46:46 +0300

    Updates for 2019.2 EAP + some cleaning

---
+++ changeSignature/demo.go
@@ -0,0 +1,11 @@
+package main
+
+// Call intention actions: click the error and press Alt+Enter.
+// Apply the suggested quick-fixes.
+func main() {
+	demo(2,"hello")
+}
+
+func demo() {
+	return 0
+}

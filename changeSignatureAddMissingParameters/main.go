994da605f1d0ea4c7f91f76596640419234dec47
Artem Pronichev <artem.pronichev@jetbrains.com>
2019-07-10 14:15:00 +0300

    changeSignatureAddParameters

---
+++ changeSignatureAddMissingParameters/main.go
@@ -0,0 +1,12 @@
+package main
+
+// Click (2, "string"), press Alt + Enter, and select 'Change parameters of function 'demo' to (int, string)'
+// Click return 0, press Alt + Enter, and select 'Add 'int' as 1st result parameter to function 'demo'
+
+func main() {
+	demo(2, "string")
+}
+
+func demo() {
+	return 0
+}

10d476162a305c365265c8a4e7397609e5912e23
Artem Pronichev <artem.pronichev@jetbrains.com>
2019-07-23 13:00:13 +0300

    Example from Florin's blogpost

--- goTemplates/main.go
+++ goTemplates/main.go
@@ -1,12 +1,56 @@
-package gcompl
+package main
 
-type PageData struct {
-	Items []Data
-	Title string
-	Name  string
-}
+import (
+	"io/ioutil"
+	"os"
+	"text/template"
+)
 
-type Data struct {
-	Greeting string
-	Info     string
+type (
+	Location struct {
+		Street  string
+		ZipCode string
+	}
+
+	User struct {
+		Username  string
+		Locations map[string]Location
+	}
+
+	UsersPage struct {
+		Title string
+		Users []User
+	}
+)
+
+func main() {
+	message, err := ioutil.ReadFile("tpl.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	t, err := template.New("UsersPage").Parse(string(message))
+	if err != nil {
+		panic(err)
+	}
+
+	p := UsersPage{
+		Title: "Users location",
+		Users: []User{
+			{
+				Username: "johnsmith",
+				Locations: map[string]Location{
+					"Home": {
+						Street:  "Main Street",
+						ZipCode: "20192",
+					},
+				},
+			},
+		},
+	}
+
+	err = t.Execute(os.Stdout, p)
+	if err != nil {
+		panic(err)
+	}
 }

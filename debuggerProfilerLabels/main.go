9267cd74e6f8dec25e962a23f867cd633187637f
Artem Pronichev <artem.pronichev@jetbrains.com>
2020-07-30 11:04:17 +0300

    initial commit

--- debuggerProfilerLabels/main.go
+++ debuggerProfilerLabels/main.go
@@ -9,17 +9,12 @@ To learn more about pprof labels, see https://rakyll.org/profiler-labels/.
 */
 import (
 	"context"
-	"math/rand"
-	"runtime/pprof"
-	"strconv"
 	"time"
 )
 
 func main() {
 	ctx := context.Background()
 	for i := 0; i < 10; i++ {
-		labels := pprof.Labels("path", "/api/profile", "userId", strconv.Itoa(rand.Intn(100)))
-		go pprof.Do(ctx, labels, f)
 		f(ctx)
 	}
 	time.Sleep(time.Second)

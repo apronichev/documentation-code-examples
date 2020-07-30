e35e9edecd2147241a287d91b57359de91d3a470
Artem Pronichev <artem.pronichev@jetbrains.com>
2020-07-30 11:09:48 +0300

    initial commit

--- debuggerProfilerLabels/main.go
+++ debuggerProfilerLabels/main.go
@@ -1,4 +1,5 @@
 package main
+
 /*
 Put a breakpoint at 'println("ok")' and debug the 'main' function. Once breakpoint is reached, check out a list of goroutines
 in the debugger. Goroutine names include the following information: /api/profile, userId: <some number>. You can use this
@@ -8,17 +9,12 @@ To learn more about pprof labels, see https://rakyll.org/profiler-labels/.
 */
 import (
 	"context"
-	"math/rand"
-	"runtime/pprof"
-	"strconv"
 	"time"
 )
 
 func main() {
 	ctx := context.Background()
 	for i := 0; i < 10; i++ {
-		labels := pprof.Labels("path", "/api/profile", "userId", strconv.Itoa(rand.Intn(100)))
-		go pprof.Do(ctx, labels, f)
 		f(ctx)
 	}
 	time.Sleep(time.Second)
